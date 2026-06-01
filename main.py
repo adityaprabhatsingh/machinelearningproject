cee473cb3cbc815142015ad45696bcd7d3af655e
aditya prabhat <adityaprabhat63@email.com>
2026-06-01 18:46:03 +0530

    exception logger and utils added

---
+++ main.py
@@ -0,0 +1,8 @@
+from venv import logger
+from mlProject import logger# ml project becaouse this is inside a mlproject folder 
+
+
+logger.info("starting the custom logging module ")## after run("[2026-06-01 17:46:55,113: INFO: main: starting the custom logging module ]") 33 and save this into lof file and also print in console
+
+
+
